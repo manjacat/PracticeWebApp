2a7ab36145a3fef827c664fcb925c69e032e06f9
Khairil RPS <manjacat@gmail.com>
2020-10-05 16:53:48 +0800

    Added First Working Code
    Added first step tutorial with working Student Model

---
+++ PracticeWebApp/Models/NorthwindContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PracticeWebApp.Models
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+        }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=NorthwindDB");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.DateOfBirth).HasColumnType("date");
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PracticeWebApp/Models/Students.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeWebApp.Models
+{
+    public partial class Students
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
